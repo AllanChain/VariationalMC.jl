1bbc89e2eb9e394dd7a0f8bccbbfdb58e4668af2
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-12-20 16:37:03 +0800

    fix element index

--- src/molecule.jl
+++ src/molecule.jl
@@ -26,7 +26,6 @@ function Molecule(atoms::Vector{Atom})
 end
 
 const ELEMENTS = [
-    "X",  # Ghost
     "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
     "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca",
     "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
