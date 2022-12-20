4a84e666df4ee9f8031f4e26ebe400b60810120f
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-12-20 11:22:15 +0800

    include check for H2 potential energy

--- test/qmc.jl
+++ test/qmc.jl
@@ -1,17 +1,18 @@
 using QMC
 using QMC.molecule
+using LinearAlgebra
 using Test
 
 
 @testset "H2 QMC" begin
+    basis = read_basis("6-31g")
+    H_basis = basis["H"]
+    molecule = Molecule([
+        Atom(1, [0.0, 0.0, 0.0], H_basis),
+        Atom(1, [1.4, 0.0, 0.0], H_basis),
+    ])
+    walker = randn(6)
     @testset "H₂ deriv params" begin
-        basis = read_basis("6-31g")
-        H_basis = basis["H"]
-        molecule = Molecule([
-            Atom(1, [0.0, 0.0, 0.0], H_basis),
-            Atom(1, [1.4, 0.0, 0.0], H_basis),
-        ])
-        walker = randn(6)
         params = init_params(molecule)
         f = log_ψ(molecule, params, walker)
         f′ = log_ψ_deriv_params(molecule, params, walker)
@@ -21,13 +22,6 @@ using Test
         @test (f2 - f) / dp ≈ f′.mo_coeff_alpha[1, 2] rtol = 1e-4
     end
     @testset "H₂ kinetic energy" begin
-        basis = read_basis("6-31g")
-        H_basis = basis["H"]
-        molecule = Molecule([
-            Atom(1, [0.0, 0.0, 0.0], H_basis),
-            Atom(1, [1.4, 0.0, 0.0], H_basis),
-        ])
-        walker = randn(6)
         params = init_params(molecule)
         f1 = exp(log_ψ(molecule, params, walker))
         ke = local_kinetic_energy(molecule, params, walker)
@@ -42,5 +36,16 @@ using Test
             laplacian += (f1 + f3 - 2 * f2) / (f2 * dx^2)
         end
         @test -1 / 2 * laplacian ≈ ke rtol = 1e-4
+    end
+    @testset "H₂ potential energy" begin
+        pe = local_potential_energy(molecule, walker)
+        mype = 1 / 1.4
+        a2 = molecule.atoms[2].coord
+        x1 = walker[begin:3]
+        x2 = walker[4:end]
+        mype +=
+            1 / norm(x1 .- x2) - 1 / norm(x1) - 1 / norm(x2) -
+            1 / norm(x1 .- a2) - 1 / norm(x2 .- a2)
+        @test pe == mype
     end
 end
